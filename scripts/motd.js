bc0526adeb53947505379c92eac3d9467829411a
Domink Sigmund <sigmund.dominik@googlemail.com>
2018-05-30 10:39:51 +0200

    Added single-File startup script

---
+++ scripts/motd.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+var config = require('./config.json')
+var execSync = require('child_process').execSync
+
+runIf(config.apc, 'sh scripts/apc.sh')
+runIf(config.zpool, 'sh scripts/zpool.sh')
+runIf(config.diskspace.use, 'node scripts/diskspace.js')
+runIf(config.hddtemp.use, 'node scripts/hddtemp.js')
+runIf(config.service.use, 'sh scripts/services.sh')
+runIf(config.ssl.use, 'node scripts/ssl.js')
+
+function runIf (condition, script) {
+  if (condition) {
+    try {
+      console.log(execSync(script).toString())
+    } 
+    catch (error) {
+      console.error(error.status)
+      console.error(error.message)
+      console.error(error.stderr.toString())
+      console.error(error.stdout.toString())
+    }
+  }
+}
